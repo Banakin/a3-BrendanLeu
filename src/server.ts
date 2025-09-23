ee3e3637fe4f427e2d370f5f67dc598e748d7649
Brendan Leu <brenleu8@gmail.com>
2025-09-23 09:49:35 -0400

    unscrew the mongodb deployment

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ const app = express();
 const port = 3000;
 
 // Database
-const client_promise = mongoose.connect(process.env.A3_STORAGE_MONGODB_URI!)
+const client_promise = mongoose.connect(process.env.MONGODB_URI!)
 .then(m => m.connection.getClient())
 
 // Session handling
