694a826188881c7d7d20b71a6c022250e02aaa22
Brendan Leu <brenleu8@gmail.com>
2025-09-25 14:40:48 -0400

    update styles

--- public/js/status.js
+++ public/js/status.js
@@ -2,8 +2,12 @@ var urlParams = new URLSearchParams(window.location.search)
 
 let state = urlParams.get('status') || 'new'
 
-if (state != 'new') {
+if (!['new', 'out'].includes(state)) {
     let notice = document.getElementById('status_notice')
+    notice.style.display = 'block'
+
+    if (state != 'fail') notice.classList.add('is-warning')
+    else notice.classList.add('is-danger')
     
     notice.innerText = `STATUS ${state}`
 
@@ -12,5 +16,4 @@ if (state != 'new') {
         notice.innerText = notice.innerText + `\nERR ${err}`
         console.warn(err)
     }
-
 }
